3b2fda55f62f84e74030b7f996c695ac9444f2ad
Masaru Kawabata <mkawabata@linkbal.co.jp>
2016-08-17 19:33:02 +0900

    add lambda-instance-operation.py

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # lambda-instance-operation
+
+This repository is AWS Lambda function's upload files.
+
+## Language
+
+- python2.7.10
+
+## Package
+
+- boto3
+
+## How To Use
+
+### 1.clone git repository
+
+```
+git clone https://github.com/mkawabata-linkbal/lambda-instance-operation.git
+```
+
+### 2.edit tag name
+
+```
+cd lambda-instance-operation
+vi lambda-instance-operation.py
+-----------
+    tag_name = "<TAG_NAME>"
+-----------
+```
+
+### 3.prepare upload zip file
+
+```
+zip -r ../lambda-instance-operation.zip .
+```
+
+### see also
+
+- http://tech.linkbal.co.jp
---
+++ lambda-instance-operation.py
@@ -0,0 +1,47 @@
+import boto3
+
+def instance_start(event, context):
+    """ Create Connection """
+    try:
+        ec2 = boto3.resource('ec2', region_name='ap-northeast-1')
+    except:
+        print('Connection Error')
+        return 1
+
+    """ Get EC2 Instance Info """
+    tag_name = "<TAG_NAME>"
+    instance = [i for i in ec2.instances.all() for t in i.tags if t["Value"] == tag_name][0]
+
+    """ Start EC2 Instance """
+    try:
+        instance.start()
+        instance.wait_until_running()
+        instance.state
+    except: 
+        print('Start EC2 Instance Error')
+        return 1
+
+    return 0
+
+def instalce_stop(event, context):
+    """ Create Connection """
+    try:
+        ec2 = boto3.resource('ec2', region_name='ap-northeast-1')
+    except:
+        print('Connection Error')
+        return 1
+
+    """ Get EC2 Instance Info """
+    tag_name = "<TAG_NAME>"
+    instance = [i for i in ec2.instances.all() for t in i.tags if t["Value"] == tag_name][0]
+
+    """ Stop EC2 Instance """
+    try:
+        instance.stop()
+        instance.wait_until_stopped()
+        instance.state
+    except: 
+        print('Stop EC2 Instance Error')
+        return 1
+
+    return 0
